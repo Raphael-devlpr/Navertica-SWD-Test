1ed5888fb074c24aa73797357e2b23042fbc60f5
Raphael <126207032+Raphael-devlpr@users.noreply.github.com>
2024-06-18 14:06:23 +0200

    Add files via upload

---
+++ SWD Test/Navertica SWD Test/Navertica SWD Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            string output = "";
+
+            if (i % 3 == 0)
+            {
+                output += "NAV";
+            }
+
+            if (i % 5 == 0)
+            {
+                output += "ERTICA";
+            }
+
+            if (output == "")
+            {
+                output = i.ToString();
+            }
+
+            Console.WriteLine(output);
+        }
+    }
+}
